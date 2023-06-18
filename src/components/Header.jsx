bb60b08d03875805feb1f418b6132bbd35dc5ab1
putubagus13 <putu031216@gmail.com>
2023-06-19 03:35:16 +0700

    integrasi profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,28 @@ import Link from 'next/link';
 import React from 'react';
 import logo from '../assets/Logo.png';
 import { HiOutlineViewList } from 'react-icons/hi';
+import http from '@/helpers/http.helper';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProfile } from '@/redux/reducers/profile';
+import User from '@/assets/user.png'
 
-function Header() {
+function Header({token}) {
+    const profile = useSelector(state => state.profile.data);
+    const dispatch = useDispatch()
+    const getProfile = React.useCallback(async()=>{
+        try {
+            const {data} = await http(token).get("/profile")
+            console.log(data.results)
+            dispatch(setProfile(data.results))
+        } catch (error) {
+            const message = error?.response?.data?.message
+            return console.log(message)
+        }
+    },[token])
+
+    React.useEffect(()=>{
+        getProfile()
+    },[getProfile])
     return (
         <nav className="fixed bg-white z-10 w-full">
             <div className="w-full h-28 flex items-center justify-between px-11 lg:px-24 xl:px-40 shadow-sm">
@@ -25,15 +45,25 @@ function Header() {
                         History
                     </Link>
                 </div>
-                <div className="flex-1 hidden md:flex items-center justify-end">
+                {!token && <div className="flex-1 hidden lg:flex items-center justify-end">
                     <Link href="/auth/login" className="btn btn-ghost rounded-full lg:w-36 text-base text-primary capitalize">
                         Login
                     </Link>
                     <Link href="/auth/register" className="btn btn-accent rounded-full lg:w-36 text-base text-primary capitalize">
                         Sign Up
                     </Link>
-                </div>
-                <div className="block md:hidden dropdown dropdown-end z-10">
+                </div>}
+                {token && <div className="flex-1 hidden lg:flex justify-end items-center gap-3">
+                    <Link href="/profile" className="w-16 h-16 overflow-hidden rounded-2xl">
+                        {profile?.picture ? (<Image width={150} height={150} className="object-fit" src={profile.picture} alt="userImage"/>) 
+                            : (<Image className="object-fit" src={User} alt="user"/>) }
+                    </Link>
+                    <div className="flex flex-col">
+                        <p className="font-bold text-secondary">{profile?.fullName}</p>
+                        <p className="font-semibold text-secondary">{profile?.email}</p>
+                    </div>
+                </div>}
+                <div className="block lg:hidden dropdown dropdown-end z-10">
                     <label tabIndex={0}>
                         <HiOutlineViewList size={30} className="text-secondary" />
                     </label>
