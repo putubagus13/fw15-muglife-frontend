3ffbbddc5bf2bcfa08fbfa53152b1f1ee3d43d75
DausHamyR <daus.amat354@gmail.com>
2023-06-18 20:26:03 +0700

    fix logout

---
+++ src/pages/auth/logout.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import React, { useCallback, useEffect, useState } from 'react'
+import { withIronSessionSsr } from "iron-session/next";
+import { useRouter } from 'next/router'
+import cookieConfig from '@/helpers/cookieConfig';
+
+function Logout() {
+    const router = useRouter()
+    const doLogout = useCallback(async()=> {
+        await axios.get('/api/logout')
+        router.replace('/auth/login')
+    }, [router])
+    useEffect(()=> {
+        doLogout()
+    }, [doLogout])
+  return (
+    <div>Logout</div>
+  )
+}
+
+export default Logout
